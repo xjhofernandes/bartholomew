da171d691665018428fb2073ec96d469f6aa8a9c
Jonathan Fernandes <jhofernandes@pop-os.localdomain>
2020-12-13 01:54:49 -0300

    Testing

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <Navbar/>
   <router-view/>
 </template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  }
+}
+</script>
 
 <style>
 
---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav class="flex justify-between bg-black p-4">
+        <div>
+            <a class="text-gray-200 italic text-1x" href="#">Bartholomew</a>
+        </div>
+        <ul class="flex flex-row">
+            <li class="pr-5 text-gray-200"><router-link to="/">Home</router-link> |</li>
+            <li class="pr-5 text-gray-200"><router-link to="/about">About</router-link></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: 'NarBar'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
