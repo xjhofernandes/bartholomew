ebb29056b6a61db9abc96cbf5ebab2748107d6d7
Jonathan Fernandes <jhofernandes@pop-os.localdomain>
2020-12-13 00:44:56 -0300

    First-commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+require('@/assets/css/tailwind.css');
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
