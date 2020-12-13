0e0b18e50a7c7f32bcc50ba62743eacb9b5ecd0e
Jonathan Fernandes <jhofernandes@pop-os.localdomain>
2020-12-13 08:21:28 -0300

    Ajustandi ico

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,11 @@
 <template>
-    <nav class="flex justify-between bg-black p-4">
-        <div>
-            <a class="text-gray-200 italic text-1x" href="#">Bartholomew</a>
-        </div>
-        <ul class="flex flex-row">
-            <li class="pr-5 text-gray-200"><router-link to="/">Home</router-link> |</li>
+    <nav class="flex justify-between bg-black p-2">
+        <ul class="flex flex-row items-center ">
+            <li><img class="h-10 pr-5" alt="Vue logo" src="../assets/logo.svg"></li>
+            <li><a class="text-gray-200 italic text-1x" href="#">Bartholomew</a></li>
+        </ul>
+        <ul class="flex flex-row items-center ">
+            <li class="pr-5 text-gray-200"><router-link to="/">Home</router-link></li>
             <li class="pr-5 text-gray-200"><router-link to="/about">About</router-link></li>
         </ul>
     </nav>
