da401004c92914091401dd1ef9660997b2db6d9f
Jonathan Fernandes <jhofernandes@pop-os.localdomain>
2020-12-13 14:46:09 -0300

    Adjusting modal

--- src/components/ModalAddJob.vue
+++ src/components/ModalAddJob.vue
@@ -23,33 +23,49 @@
               </p>
             </div>
             <div class="mt-2">
+              <p>Name:</p>
+              <input class="bg-gray-100 focus:ring-2 focus:ring-gray-300 rounded-md h-8 text-2xl" type="text">
+            </div>
+            <div class="mt-2">
               <p>Endpoint:</p>
               <input class="bg-gray-100 focus:ring-2 focus:ring-gray-300 rounded-md h-8 text-2xl" type="text">
-              <div id="v-model-select-dynamic" class="demo">
-              <select v-model="selected">
+            </div>
+            <div class="mt-2">
+              <p>Method:</p>
+              <select class="bg-gray-100 focus:ring-2 focus:ring-gray-300 rounded-md h-8 text-2xl" v-model="selected">
                 <option disabled value="">Please select one</option>
                 <option v-for="option in options" :key="option" :value="option.value">
                   {{ option.text }}
                 </option>
               </select>
-              <span>Selected: {{ selected }}</span>
             </div>
-
+            <div class="mt-2">
+              <p>Cron Expression:</p>
+              <input class="bg-gray-100 focus:ring-2 focus:ring-gray-300 rounded-md h-8 text-2xl" type="text">
+            </div>
+            <div class="mt-2">
+              <p>Criticality:</p>
+              <select class="bg-gray-100 focus:ring-2 focus:ring-gray-300 rounded-md h-8 text-2xl" v-model="criticality" required>
+                <option disabled value="">Please select one</option>
+                <option v-for="criticalit in criticalitys" :key="criticalit" :value="criticalit.value">
+                  {{ criticalit.text }}
+                </option>
+              </select>
             </div>
           </div>
+          </div>
+        </div>
+        <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+          <button type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
+            Save
+          </button>
+          <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+            Cancel
+          </button>
         </div>
       </div>
-      <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-        <button type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
-          Save
-        </button>
-        <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
-          Cancel
-        </button>
-      </div>
     </div>
-        </div>
-    </div>
+  </div>
 </template>
 
 <script>
@@ -61,6 +77,12 @@ export default {
       options: [
         { text: 'Get', value: 'get' },
         { text: 'Post', value: 'post' }
+      ],
+      criticality: 'high',
+      criticalitys: [
+        {text : 'High', value: 'high'},
+        {text : 'Medium', value: 'medium'},
+        {text : 'Low', value: 'low'}
       ]
     }
   }
